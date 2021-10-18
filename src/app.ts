0ec4c37939a08dd5a7a73a57861e0324a018696f
karenmrdn <karenmrdn47@gmail.com>
2021-10-18 11:18:28 +0300

    Strict options as well as some additional options enabled

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
+let appId = "123"; // ts is not able to detect if it is used or not, because it can be used in another file
+
 const button = document.querySelector("button")!;
 
-button.addEventListener("click", () => {
-  console.log("Clicked!");
-});
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+function clickHandler(message: string) {
+  // let username = "Andrew"; // ts can detect if it is used or not because it is local variable
+  console.log("Clicked! " + message);
+}
+
+// Some comment
+if (button) {
+  button.addEventListener("click", clickHandler.bind(this, "You are welcome"));
+}
